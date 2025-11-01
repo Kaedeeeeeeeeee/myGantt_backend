49e03cbf9de10920c6847e3d958bf647f53fb81c
Kaedeeeeeeeeee <f.shera.09@gmail.com>
2025-11-01 23:13:51 +0900

    Restore backend files from previous commit

--- src/utils/invitation.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 生成邀请链接
- */
-export const generateInvitationLink = (token: string): string => {
-  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5174';
-  return `${frontendUrl}/invitation/${token}`;
-};
-
